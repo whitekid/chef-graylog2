c2f1c25c323b7c9301fee1edf43ed447061a4b4f
Sean Porter <portertech@gmail.com>
2011-06-27 18:41:51 -0700

    collection size attribute, dot notation and underscore

--- cookbooks/graylog2/attributes/default.rb
+++ cookbooks/graylog2/attributes/default.rb
@@ -1,4 +1,4 @@
 default.graylog2.basedir = "/var/graylog2"
 default.graylog2.server.version = "0.9.5p1"
 default.graylog2[:webuiversion] = "0.9.4p2"
-default.graylog2[:collectionsize] = "50000000"
+default.graylog2.collection_size = "50000000"
