a9653823a9da7097f67ad09a5cd860da6c6eeff9
Sean Porter <portertech@gmail.com>
2011-06-27 18:32:59 -0700

    cut the fat, use a release dir, dot notation attributes, bump server version

--- cookbooks/graylog2/attributes/default.rb
+++ cookbooks/graylog2/attributes/default.rb
@@ -1,4 +1,4 @@
-default.graylog2[:basedir] = "/var/graylog2"
-default.graylog2[:serverversion] = "0.9.4p1"
+default.graylog2.basedir = "/var/graylog2"
+default.graylog2.server.version = "0.9.5p1"
 default.graylog2[:webuiversion] = "0.9.4p2"
 default.graylog2[:collectionsize] = "50000000"
--- cookbooks/graylog2/recipes/server.rb
+++ cookbooks/graylog2/recipes/server.rb
@@ -17,79 +17,69 @@
 # limitations under the License.
 #
 
-# Add apt public key for mongodb repo
+# Add APT public key for the 10gen MongoDB repo
 execute "get_mongodb_pubkey" do
   command "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
   not_if 'apt-key list | grep "7F0CEB10"'
-  action :nothing
 end
 
-# Add mongodb repository to apt
+# Add the 10gen MongoDB repository to APT
 apt_repository "mongodb" do
   uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
   distribution "dist"
   components ["10gen"]
   action :add
-  notifies :run, "execute[get_mongodb_pubkey]", :immediately
 end
 
-# Install required apt packages
+# Install required APT packages
 package "openjdk-6-jre"
 package "mongodb-10gen"
 
-# Create application directory
-directory "#{node[:graylog2][:basedir]}/src" do
-  owner "root"
-  group "root"
+# Create the release directory
+directory "#{node.graylog2.basedir}/rel" do
   mode 0755
-  action :create
-  recursive true
 end
 
-# Use remote_file to grab the desired version of graylog2-server package
+# Download the desired version of Graylog2 server from GitHub
 remote_file "graylog2_server" do
-  path "#{node[:graylog2][:basedir]}/src/graylog2-server-#{node[:graylog2][:serverversion]}.tar.gz"
-  source "https://github.com/downloads/Graylog2/graylog2-server/graylog2-server-#{node[:graylog2][:serverversion]}.tar.gz"
+  path "#{node.graylog2.basedir}/rel/graylog2-server-#{node.graylog2.server.version}.tar.gz"
+  source "https://github.com/downloads/Graylog2/graylog2-server/graylog2-server-#{node.graylog2.server.version}.tar.gz"
   action :create_if_missing
 end
 
-# Unpack graylog2-server
+# Unpack the desired version of Graylog2 server
 execute "unpack_graylog2_server" do
-  cwd "#{node[:graylog2][:basedir]}/src"
-  command "tar zxf graylog2-server-#{node[:graylog2][:serverversion]}.tar.gz"
-  creates "#{node[:graylog2][:basedir]}/src/graylog2-server-#{node[:graylog2][:serverversion]}/build_date"
+  cwd "#{node.graylog2.basedir}/rel"
+  command "tar zxf graylog2-server-#{node.graylog2.server.version}.tar.gz"
+  creates "#{node.graylog2.basedir}/rel/graylog2-server-#{node.graylog2.server.version}/build_date"
   subscribes :run, resources(:remote_file => "graylog2_server"), :immediately
 end
 
-# Link graylog2-server
-link "#{node[:graylog2][:basedir]}/server" do
-  to "#{node[:graylog2][:basedir]}/src/graylog2-server-#{node[:graylog2][:serverversion]}"
+# Link to the desired Graylog2 server version
+link "#{node.graylog2.basedir}/server" do
+  to "#{node.graylog2.basedir}/rel/graylog2-server-#{node.graylog2.server.version}"
 end
 
 # Create graylog2.conf
 template "/etc/graylog2.conf" do
   source "graylog2.conf.erb"
-  owner "root"
-  group "root"
   mode 0644
 end
 
 # Create init.d script
 template "/etc/init.d/graylog2" do
   source "graylog2.init.erb"
-  owner "root"
-  group "root"
   mode 0755
 end
 
 # Update the rc.d system
 execute "update-rc.d graylog2 defaults" do
   creates "/etc/rc0.d/K20graylog2"
-  notifies :start, "service[graylog2]", :delayed
+  subscribes :run, resources(:template => "/etc/init.d/graylog2"), :immediately
 end
 
 # Service resource
 service "graylog2" do
   supports :restart => true
-  action :enable
+  action [:enable, :start]
 end
