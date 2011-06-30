58bac25fae8ddf15bca7913c751f99946e9cf08e
Sean Porter <portertech@gmail.com>
2011-06-29 17:20:12 -0700

    bump cookbook version

--- cookbooks/graylog2/metadata.rb
+++ cookbooks/graylog2/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Medidata Solutions Inc."
 maintainer_email  "cloudteam@mdsol.com"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
-version           "0.0.4"
+version           "0.0.5"
 recipe            "graylog2", "Installs and configures Graylog2"
 
 # Only supporting Ubuntu 10.x
