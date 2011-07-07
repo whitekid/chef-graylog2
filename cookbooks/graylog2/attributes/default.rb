dee18584b21d5db419173bb20c4646b19f89d099
Sean Porter <portertech@gmail.com>
2011-07-07 11:45:32 -0700

    graylog2 email attrib, more dot notation

--- cookbooks/graylog2/attributes/default.rb
+++ cookbooks/graylog2/attributes/default.rb
@@ -1,7 +1,7 @@
 default.graylog2.basedir = "/var/graylog2"
 default.graylog2.server.version = "0.9.5p1"
 default.graylog2.web_interface.version = "0.9.5p2"
-default.graylog2.port = 514
+
 default.graylog2.mongodb.host = "localhost"
 default.graylog2.mongodb.port = 27017
 default.graylog2.mongodb.max_connections = 500
@@ -9,7 +9,10 @@ default.graylog2.mongodb.database = "graylog2"
 default.graylog2.mongodb.auth = "false"
 default.graylog2.mongodb.user = "user"
 default.graylog2.mongodb.password = "password"
+
+default.graylog2.port = 514
 default.graylog2.collection_size = 50000000
+default.graylog2.email = "graylog2@example.org"
 default.graylog2.send_stream_alarms = true
 default.graylog2.send_stream_subscriptions = true
 default.graylog2.stream_alarms_cron_minute = "*/15"
